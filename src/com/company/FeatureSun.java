64ad1971fd531e788613a6c4c04d12c5991a2aac
mlm-25 <youngmauri43@gmail.com>
2021-11-06 11:54:18 -0400

    feature Sun added with a bug that needs to be resolved

---
+++ src/com/company/FeatureSun.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class FeatureSun {
+
+    String heat;
+    String temp;
+    int radius;
+    String color;
+    String martialStatus;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,12 @@ public class Main {
         andromeda.planet = 2;
         andromeda.moon = 4;
 
+        FeatureSun mikeSun = new FeatureSun();
+        mikeSun.color = "blue";
+        mikeSun.heat = "60000003434kj";
+        mikeSun.temp = "5 Celsius";
+        mikeSun.radius = "0.0009";
+        mikeSun.martialStatus = "single";
+
     }
 }
