b3c6a93fd31938a70bfe4fa304ac6be8d780211b
mlm-25 <youngmauri43@gmail.com>
2021-11-06 10:56:31 -0400

    Solar System Basic Structure is Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        SolarSystem andromeda = new SolarSystem();
+        andromeda.sun = 1;
+        andromeda.planet = 2;
+        andromeda.moon = 4;
+
+    }
+}
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SolarSystem {
+
+    int sun;
+    int planet;
+    int moon;
+
+}
